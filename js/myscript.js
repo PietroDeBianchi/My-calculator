c0f6ed2cfc1b79a642e81b3749cb2554f9abda26
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-02-26 19:29:41 +0100

    start building with javascript

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
+
+let input = "";
